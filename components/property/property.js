c25de81588602660619da775e98b7cc096da68c9
Scott Sanford <ssanford@mediafly.com>
2016-02-28 21:30:27 -0600

    Get data to property view

--- components/property/property.js
+++ components/property/property.js
@@ -1,6 +1,19 @@
 angular.module('ggpApp')
 
-.controller('PropertyCtrl', function($scope){
+.controller('PropertyCtrl', function($scope, $routeParams, mfly){
 
+	var propertyID = $routeParams.id;
+	var dataId = 'a4ce3ae64bb34998bd28479d8b7f8201product234567';
+
+	mfly.getData(dataId).then(function(data){
+		var jsonData = JSON.parse(data);
+
+		jsonData.forEach(function(value,key){
+			if (propertyID === value.id) {
+				console.log(value);
+				$scope.mall = value;
+			}
+		})
+	});
 
 });
