934ac0b0e84b1ec70cf2a7756d56429e51b658fb
Scott Sanford <ssanford@mediafly.com>
2016-03-16 17:29:23 -0500

    Add meeting from Dialog to Calendar View

--- components/meetings/meetings.js
+++ components/meetings/meetings.js
@@ -1,16 +1,15 @@
 angular.module('ggpApp')
 
-.controller('MeetingsCtrl', function($scope, mfly, propertyData, moment, ngDialog){
-
-
+.controller('MeetingsCtrl', function($scope, $rootScope, mfly, propertyData, moment, ngDialog){
 
 
     //These variables MUST be set as a minimum for the calendar to work
     $scope.calendarView = 'month';
     $scope.viewDate = new Date();
-    $scope.events = [
+
+    var eventList = [
 	  {
-        title: 'This is a really long event title that occurs on every year',
+        title: 'Gap Meeting',
         type: 'important',
         startsAt: moment().startOf('day').add(7, 'hours').toDate(),
         endsAt: moment().startOf('day').add(19, 'hours').toDate(),
@@ -21,7 +20,44 @@ angular.module('ggpApp')
       }
     ];
 
-    $scope.isCellOpen = true;
+    $scope.events = eventList;
+
+    $scope.addMeeting = function() {
+    	var meeting = {
+    		title: 'New event', 
+    		type: 'important', 
+    		draggable: true, 
+    		resizable: true
+    	}
+    	$scope.events.push(meeting);
+    }
+
+    $scope.isCellOpen = false;
+
+    $scope.createMeeting = function() {
+		ngDialog.open({ 
+			template: 'common/tmpls/dialogs/createMeeting.html', 
+			className: 'ngdialog-theme-default', 
+			scope: $scope,
+			controller: function($scope) {
+			    $scope.toggle = function($event, field, event) {
+			      $event.preventDefault();
+			      $event.stopPropagation();
+			      event[field] = !event[field];
+			    };
+
+			    $scope.addMeetingToCalendar = function(meeting) {
+			    	meeting['recursOn']    = 'year';
+			    	meeting['draggable']   = false;
+			    	meeting['resizeable']  = true;
+
+			    	$scope.events.push(meeting);
+			    	$scope.closeThisDialog();
+
+			    }
+			}
+		});
+    };
 
     $scope.eventClicked = function(event) {
       console.log('Clicked', event);
@@ -39,10 +75,5 @@ angular.module('ggpApp')
       console.log('Dropped or resized', event);
     };
 
-    $scope.toggle = function($event, field, event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-      event[field] = !event[field];
-    };
 
 });
