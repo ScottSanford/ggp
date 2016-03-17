9527c6d4f65c13051c40aaf09a26a47ee77e4cc2
Scott Sanford <ssanford@mediafly.com>
2016-03-17 14:28:30 -0500

    Add Extra Filters

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -16,6 +16,8 @@ angular.module('ggpApp')
 			});
 		});
 
+		console.log("Properties --> ", propertyData);
+
 		$scope.malls = propertyData;
 
 	});
@@ -27,14 +29,50 @@ angular.module('ggpApp')
 		}, 
 		minStart: 0, 
 		maxEnd: 3000000
-	};
+	};	
 
-	$scope.byRange = function (minValue, maxValue) {
+	$scope.filterGLA = function (minValue, maxValue) {
 	  	minValue === undefined ? 0 : minValue;
   		maxValue === undefined ? 100000000 : maxValue;
 
 	  	return function predicateFunc(item) {
 	    	return minValue <= item.overview.gla && item.overview.gla <= maxValue;
+	  	};
+	};	
+
+	$scope.median = {
+		range: {
+			min: 0, 
+			max: 115000
+		}, 
+		minStart: 0, 
+		maxEnd: 115000
+	};
+
+	$scope.filterMedian = function (minValue, maxValue) {
+	  	minValue === undefined ? 0 : minValue;
+  		maxValue === undefined ? 100000000 : maxValue;
+
+	  	return function predicateFunc(item) {
+	    	return minValue <= item.tradeArea.avg_income && item.tradeArea.avg_income <= maxValue;
+	  	};
+	};	
+
+	$scope.stores = {
+		range: {
+			min: 0, 
+			max: 500
+		}, 
+		minStart: 0, 
+		maxEnd: 500
+	};
+
+	$scope.filterStores = function (minValue, maxValue) {
+	  	minValue === undefined ? 0 : minValue;
+  		maxValue === undefined ? 100000000 : maxValue;
+
+	  	return function predicateFunc(item) {
+	    	return minValue <= item.overview.stores && item.overview.stores <= maxValue;
 	  	};
 	};
 
